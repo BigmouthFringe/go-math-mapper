b8cbbf8049b53838e8e7394c7b3849e6a93de1ba
Alexey Rudi <bigmouthfringe@gmail.com>
2019-07-14 13:42:10 +0700

    Add input handling

--- cmd/go_math_mapper/main.go
+++ cmd/go_math_mapper/main.go
@@ -1,7 +1,16 @@
 package main
 
+import (
+	"../../internal/pkg/json_hndl"
+	"fmt"
+)
+
 func main() {
-	// TODO: Add input receiving here
+	var outFile string
+	json_hndl.HandleJSON(func(jobs []json_hndl.Job, output string) {
+		outFile = output
+		fmt.Println(jobs, outFile)
+	})
 	// TODO: Add input mapping here
 	// TODO: Add output extracting here
 }
---
+++ internal/pkg/json_hndl/json_hndl.go
@@ -0,0 +1,50 @@
+// This package handles user console input
+// consisting of input and output file names.
+// Specifically, it decodes JSON and converts it to Jobs.
+package json_hndl
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+type Job struct {
+	Arg1 int
+	Arg2 int
+}
+
+// Client of this function should provide callback that
+// will be called each time user input successfully handled.
+// It will ignore Jobs that don't match Job struct spec
+// TODO: Write tests
+func HandleJSON(callback func(jobs []Job, output string)) {
+	fmt.Println("Please specify input and output files in respective order")
+	fmt.Println("Mind that input file should be in JSON format")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		inpParams := strings.Split(scanner.Text(), " ")
+		if len(inpParams) < 2 {
+			fmt.Println("unable to work with less than 2 arguments")
+			continue
+		}
+
+		input, output := inpParams[0], inpParams[1]
+		file, err := ioutil.ReadFile(input)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		var jobs []Job
+		err = json.Unmarshal([]byte(file), &jobs)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+		callback(jobs, output)
+	}
+}
