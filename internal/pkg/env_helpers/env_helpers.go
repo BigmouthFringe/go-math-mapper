c7ea972deee37632805916b7076e554dd4986aa8
Alexey Rudi <bigmouthfringe@gmail.com>
2019-07-17 21:32:56 +0700

    Fix executable load error

--- internal/pkg/env_helpers/env_helpers.go
+++ internal/pkg/env_helpers/env_helpers.go
@@ -2,12 +2,20 @@ package env_helpers
 
 import (
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
 
-func GetGOPATH() string {
-	return strings.Split(os.Getenv("GOPATH"), ";")[0]
+// TODO: Find an idiomatic way to determine whether it's temp debug build or executable
+func GetRootPath() string {
+	projGOPATH := strings.Split(os.Getenv("GOPATH"), ";")[0]
+	if !strings.Contains(projGOPATH, "go-math-mapper") {
+		exe, _ := os.Executable()
+		exeDir := filepath.Dir(exe)
+		return exeDir
+	}
+	return projGOPATH
 }
 
 func GetOsBitVersion() int {
